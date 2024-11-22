ada19af745e7780224a88f57fb741407f19e10d4
jipyy <jahdanpandita16@gmail.com>
2024-11-22 08:57:24 +0700

    a

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -33,7 +33,7 @@ class TutorialController extends Controller
     {
         // Path ke folder 'storage/app/public/vidio'
         // $videoDirectory = storage_path('app/public/vidio');
-        $videoDirectory = public_path('public/vidio/');
+        $videoDirectory = public_path('vidio/');
         // $videoDirectory = url('/vidio/tutorial_mengajukan_permintaan_sebagai_penjual.mp4');
         // $videoDirectory = url('/vidio');
 
