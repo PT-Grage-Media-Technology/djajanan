1e5af1130a168ef5b44341960f57cc6ffb48f812
ArvinoDel <andikasupriyadi27@gmail.com>
2024-11-25 15:09:19 +0700

    sdfb

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -119,6 +119,7 @@ class ProductController extends Controller
 
         // Mengambil data produk dengan pagination dinamis
         $products = Product::with('category', 'toko')->paginate($itemsPerPage);
+        dd($products);
 
         // Jika permintaan AJAX, kembalikan data produk dalam format JSON
         if ($request->ajax()) {
