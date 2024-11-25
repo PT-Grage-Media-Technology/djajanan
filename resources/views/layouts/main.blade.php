39cf1d8b46016bda0e0a672a9e3d16807f7632e2
ArvinoDel <andikasupriyadi27@gmail.com>
2024-11-25 15:31:58 +0700

    njkb

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -97,7 +97,7 @@
     @yield('script')
 
     <script defer src="https://rawcdn.githack.com/gragemediatechnology/keyFood/ea622abc460ff8dd056dcec020989ee66a3f878e/public_html/js/clock.js"></script>
-    <script defer src="https://rawcdn.githack.com/gragemediatechnology/keyFood/ea622abc460ff8dd056dcec020989ee66a3f878e/public_html/js/cart.js"></script>
+    <script defer src="https://rawcdn.githack.com/PT-Grage-Media-Technology/djajanan/9d3733e7c2f0a83eaf0e90577c8edb80c13f7e41/public/js/cart.js"></script>
     <script defer src="https://rawcdn.githack.com/gragemediatechnology/keyFood/ea622abc460ff8dd056dcec020989ee66a3f878e/public_html/js/home.js"></script>
     <script defer src="https://rawcdn.githack.com/gragemediatechnology/keyFood/ea622abc460ff8dd056dcec020989ee66a3f878e/public_html/js/load.js"></script>
     <script defer src="https://rawcdn.githack.com/gragemediatechnology/keyFood/46ef1975afc1dce417ab00c89c9161e4f81c52e0/public_html/js/home-container.js"></script>
