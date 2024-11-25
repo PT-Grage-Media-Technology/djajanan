c313e5309f9850027fe891170ecf34ce73edf376
ArvinoDel <andikasupriyadi27@gmail.com>
2024-11-25 13:31:11 +0700

    mmnm

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -96,7 +96,7 @@
                         </div>
                     </div>
                     <!-- Nomor Dropdown -->
-                    {{-- <div class="form-control">
+                    <div class="form-control" hidden>
                         <label for="nomor-select">Pilih Nomor</label>
                         <div>
 
@@ -104,9 +104,9 @@
                                 <option value="" disabled selected>Pilih Nomor...</option>
                             </select>
                             <input type="tel" name="nomor_id" id="nomor-select"
-                            placeholder="Masukkan Alamat Anda..." required>
+                            placeholder="Masukkan Alamat Anda..." value=" " required>
                         </div>
-                    </div> --}}
+                    </div>
                     <!-- Notes Input -->
                     <div class="form-control">
                         <label for="checkout-notes">Catatan</label>
