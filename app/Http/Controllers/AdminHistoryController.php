ae9ae505cb6a7082201c37a9eb22d7f940c2c5dc
jipyy <jahdanpandita16@gmail.com>
2024-12-09 12:06:51 +0700

    ww

--- app/Http/Controllers/AdminHistoryController.php
+++ app/Http/Controllers/AdminHistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AdminHistory;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminHistoryController extends Controller
@@ -10,6 +11,13 @@ class AdminHistoryController extends Controller
     public function index()
     {
         $histories = AdminHistory::with('admin')->latest()->paginate(1000);
+
+        $histories = User::all(); // Mengambil semua user tanpa soft delete
+
+        $histories = User::with('roles')->get();
+        return view('admin.history.index', compact('histories'));
+
+        $histories = User::all();
         return view('admin.history.index', compact('histories'));
     }
 }
--- resources/views/admin/history/index.blade.php
+++ resources/views/admin/history/index.blade.php
@@ -34,8 +34,7 @@
                                 <th class="px-4 py-3">Email</th>
                                 <th class="px-4 py-3">Action</th>
                                 <th class="px-4 py-3">Model</th>
-                                <th class="px-4 py-3">Action</th>
-                                <th class="px-4 py-3">Changes</th>
+                                <th class="px-4 py-3">Role</th>
                                 <th class="px-4 py-3">Date</th>
                             </tr>
                         </thead>
@@ -44,13 +43,20 @@
                                 <tr class="text-gray-700 dark:text-gray-400">
                                     <td class="px-4 py-3 text-sm"> {{ $history->admin->name }}</td>
                                     <td class="px-4 py-3 text-sm"> {{ $history->admin->email }}</td>
-                                    <td class="px-4 py-3 text-sm"> {{ $history->action }}</td>
+                                    <td class="px-4 py-3 text-sm"> {{ $history->action }}</td>  
                                     <td class="px-4 py-3 text-sm"> {{ $history->affected_model }}</td>
-                                    <td class="px-4 py-3 text-sm"> {{ $history->admin->email }}</td>
-                                    <td class="px-4 py-3 text-sm"> {{ $history->changes }} </td>
+                                    <td class="px-4 py-3 text-sm">
+                                        @if ($history->roles->isEmpty())
+                                            buyer
+                                        @else
+                                            @foreach ($history->roles as $role)
+                                                {{ $role->name }}@if (!$loop->last)
+                                                    ,
+                                                @endif
+                                            @endforeach
+                                        @endif
+                                    </td>
                                     <td class="px-4 py-3 text-sm"> {{ $history->created_at->format('Y-m-d H:i:s') }} </td>
-
-
                                 </tr>
                             @endforeach
                         </tbody>
