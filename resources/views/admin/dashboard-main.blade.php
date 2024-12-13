6d53a9e156b1b3c1ce3fce091a6bcbbdf8f14398
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-13 11:38:35 +0700

    asa

--- resources/views/admin/dashboard-main.blade.php
+++ resources/views/admin/dashboard-main.blade.php
@@ -204,7 +204,7 @@
                 const colors = [];
                 for (let i = 0; i < count; i++) {
                     const red = Math.floor(Math.random() * 256);
-                    const green = Math.floor(Math.random() * 256);
+                    // const green = Math.floor(Math.random() * 256);
                     const blue = Math.floor(Math.random() * 256);
                     colors.push(`rgb(${red}, ${green}, ${blue})`);
                 }
