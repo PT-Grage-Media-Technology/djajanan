1833ae58a1da88421ee6c5abce7eb43ca772bcf2
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-19 10:11:00 +0700

    sadasd

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -253,8 +253,8 @@
                             </table>
                         </div>
                     </div>
-                    <div class="flex items-center justify-center py-5 lg:px-0 sm:px-6 px-4">
-                        <div class="flex justify-between items-center mb-5">
+                    <div class="flex items-center justify-center px-4 mb-10">
+                        <div class="flex justify-between items-center">
                             <!-- Previous Page -->
                             @if ($currentPage > 1)
                                 <a href="?page={{ $currentPage - 1 }}" class="text-blue-500 hover:text-blue-700">Previous</a>
