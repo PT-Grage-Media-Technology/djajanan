79f2cc338f1b7d435973f76ba80c65a159fff12c
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-19 10:57:40 +0700

    hbkjbk

--- app/Http/Controllers/AdminHistoryController.php
+++ app/Http/Controllers/AdminHistoryController.php
@@ -9,7 +9,19 @@ class AdminHistoryController extends Controller
 {
     public function index()
     {
-        $histories = AdminHistory::with('admin')->latest()->paginate(1000);
-        return view('admin.history.index', compact('histories'));
+        $perPage = 10;
+        $currentPage = request()->get('page', 1);  // Ambil halaman saat ini, default 1
+
+        // Eager loading 'admin' dengan 'with' dan menerapkan pagination
+        $histories = AdminHistory::with('admin')  // Eager load relasi 'admin'
+            ->skip(($currentPage - 1) * $perPage)   // Menentukan halaman
+            ->take($perPage)                        // Menentukan jumlah data per halaman
+            ->get();
+
+        $total = AdminHistory::count();  // Total data AdminHistory
+
+        $lastPage = ceil($total / $perPage);  // Hitung jumlah halaman
+
+        return view('admin.history.index', compact('histories', 'currentPage', 'lastPage', 'perPage'));
     }
 }
