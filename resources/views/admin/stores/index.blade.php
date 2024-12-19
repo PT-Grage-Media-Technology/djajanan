790fcb7c454fad100fe99f06d6ebe24779ee6a4a
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-19 10:50:33 +0700

    jkjgjk

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -241,74 +241,70 @@
                                 </td>
                                 </tr>
                             @endforeach
-                            <div class="flex items-center justify-center px-4 mb-10">
-                                <nav class="flex items-center gap-x-2" aria-label="Pagination">
-                                    <!-- Previous Page -->
-                                    @if ($currentPage > 1)
-                                        <a href="?page={{ $currentPage - 1 }}"
-                                            class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-gray-300 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:border-transparent dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-                                            aria-label="Previous">
-                                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg"
-                                                width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path d="m15 18-6-6 6-6"></path>
-                                            </svg>
-                                            <span class="sr-only">Previous</span>
-                                        </a>
-                                    @else
-                                        <span
-                                            class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent text-gray-400 cursor-not-allowed">
-                                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg"
-                                                width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path d="m15 18-6-6 6-6"></path>
-                                            </svg>
-                                            <span class="sr-only">Previous</span>
-                                        </span>
-                                    @endif
-
-                                    <!-- Page Buttons -->
-                                    <div class="flex items-center gap-x-2">
-                                        @for ($i = 1; $i <= $lastPage; $i++)
-                                            <a href="?page={{ $i }}"
-                                                class="min-h-[38px] min-w-[38px] flex justify-center items-center border text-sm py-2 px-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500
-                                               @if ($i == $currentPage) bg-teal-600 text-white border-teal-600 @else border-gray-300 text-gray-800 hover:bg-gray-100 dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10 @endif">
-                                                {{ $i }}
-                                            </a>
-                                        @endfor
-                                    </div>
-
-                                    <!-- Next Page -->
-                                    @if ($currentPage < $lastPage)
-                                        <a href="?page={{ $currentPage + 1 }}"
-                                            class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-gray-300 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:border-transparent dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-                                            aria-label="Next">
-                                            <span class="sr-only">Next</span>
-                                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg"
-                                                width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path d="m9 18 6-6-6-6"></path>
-                                            </svg>
-                                        </a>
-                                    @else
-                                        <span
-                                            class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent text-gray-400 cursor-not-allowed">
-                                            <span class="sr-only">Next</span>
-                                            <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg"
-                                                width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path d="m9 18 6-6-6-6"></path>
-                                            </svg>
-                                        </span>
-                                    @endif
-                                </nav>
-                            </div>
                         </tbody>
                     </table>
+                    <div class="flex items-center justify-center px-4 mb-10">
+                        <nav class="flex items-center gap-x-2" aria-label="Pagination">
+                            <!-- Previous Page -->
+                            @if ($currentPage > 1)
+                                <a href="?page={{ $currentPage - 1 }}"
+                                    class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-gray-300 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:border-transparent dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+                                    aria-label="Previous">
+                                    <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="m15 18-6-6 6-6"></path>
+                                    </svg>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                            @else
+                                <span
+                                    class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent text-gray-400 cursor-not-allowed">
+                                    <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="m15 18-6-6 6-6"></path>
+                                    </svg>
+                                    <span class="sr-only">Previous</span>
+                                </span>
+                            @endif
+
+                            <!-- Page Buttons -->
+                            <div class="flex items-center gap-x-2">
+                                @for ($i = 1; $i <= $lastPage; $i++)
+                                    <a href="?page={{ $i }}"
+                                        class="min-h-[38px] min-w-[38px] flex justify-center items-center border text-sm py-2 px-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500
+                                       @if ($i == $currentPage) bg-teal-600 text-white border-teal-600 @else border-gray-300 text-gray-800 hover:bg-gray-100 dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10 @endif">
+                                        {{ $i }}
+                                    </a>
+                                @endfor
+                            </div>
+
+                            <!-- Next Page -->
+                            @if ($currentPage < $lastPage)
+                                <a href="?page={{ $currentPage + 1 }}"
+                                    class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-gray-300 text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:border-transparent dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+                                    aria-label="Next">
+                                    <span class="sr-only">Next</span>
+                                    <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="m9 18 6-6-6-6"></path>
+                                    </svg>
+                                </a>
+                            @else
+                                <span
+                                    class="flex items-center justify-center min-h-[38px] min-w-[38px] py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg border border-transparent text-gray-400 cursor-not-allowed">
+                                    <span class="sr-only">Next</span>
+                                    <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="m9 18 6-6-6-6"></path>
+                                    </svg>
+                                </span>
+                            @endif
+                        </nav>
+                    </div>
                 </div>
                 <script>
                     function confirmDelete() {
